11304eff6fcafe9d78d4596032c7d84f9d5673d1
JunHatanaka <nextseason6@gmail.com>
2021-12-24 01:10:49 +0900

    Change: Changed after copy action alert to change innerHTML.

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,9 @@ export default function Home() {
     if (process.browser) {
     const copyText = document.getElementById("copyTarget").innerText;
     await navigator.clipboard.writeText(copyText)
-    alert(`IDをコピーしました\nID: ` + copyText);
+    const copyButton = document.getElementById("copy-button");
+    copyButton.innerHTML = 'Copied!'
+    setTimeout(() => copyButton.innerHTML = 'Copy', 1000)
     }
   }
 
@@ -47,7 +49,7 @@ export default function Home() {
         <p id="copyTarget" className="text-5xl font-bold">{randStrAndRandNum}</p>
       </div>
       <div className="flex justify-center m-3">
-        <button onClick={copyToClipboard} className="m-1 px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:bg-blue-500">Copy</button>
+        <button id="copy-button" onClick={copyToClipboard} className="m-1 px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:bg-blue-500">Copy</button>
         <button onClick={reloadStr} className="m-1 px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:bg-blue-500">Reload</button>
       </div>
     </div>
