79a7733f36df81131dc0332678ce97121376871f
JunHatanaka <nextseason6@gmail.com>
2021-12-27 03:17:10 +0900

    Add: Added Password length settings.

--- components/Password.jsx
+++ components/Password.jsx
@@ -2,13 +2,21 @@ import { useState, useEffect } from "react";
 
 export default function RandPassword() {
     const [password, setPassword] = useState("");
+    const [strNums, setStrNums] = useState([]);
+    const [strNum, setStrNum] = useState("8");
+
 
     useEffect(() => {
         setPassword(getPasswordStr());
-    }, []);
+        setStrNums([...Array(9)].map((_, i) => i + 4));
+    }, [strNum]);
+
+    const getSelectedStrNum = (event) => {
+        setStrNum(event.target.value);
+    };
     
     const getPasswordStr = () => {
-        const numberOfDigits = 10;
+        const numberOfDigits = strNum;
         const useCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         const useCharactersLength = useCharacters.length;
         let actualStr = "";
@@ -35,13 +43,30 @@ export default function RandPassword() {
     return (
         <>
             <div className="h-10 leading-10">
-            <div className="flex justify-center m-5">
-                <p id="copyTarget" className="text-5xl font-bold">{password}</p>
-            </div>
-            <div className="flex justify-center m-3">
-                <button id="copy-button" onClick={copyToClipboard} className="m-1 px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:bg-blue-500">Copy</button>
-                <button onClick={reloadStr} className="m-1 px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:bg-blue-500">Reload</button>
-            </div>
+                <div className="flex justify-center m-5">
+                    {password.length > 10 &&
+                        <p id="copyTarget" className="text-4xl font-bold">{password}</p>
+                    }
+                    {password.length <= 10 &&
+                        <p id="copyTarget" className="text-5xl font-bold">{password}</p>
+                    }
+                </div>
+                <div className="flex justify-center m-3">
+                    <button id="copy-button" onClick={copyToClipboard} className="m-1 px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:bg-blue-500">Copy</button>
+                    <button onClick={reloadStr} className="m-1 px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:bg-blue-500">Reload</button>
+                </div>
+                <div className="flex justify-center m-3">
+                    <div className="mb-8">
+                        <label htmlFor="name" className="text-sm block">Numbers length</label>
+                        <select className="add-input-style" name="" id="" value={strNum} onChange={getSelectedStrNum}>
+                            {strNums.map((strNum, index)=>{
+                                return (
+                                    <option key={index} id="alphabets-word" className="w-full py-2 border-b focus:outline-none focus:border-b-2 focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50" value={strNum}>{strNum}</option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                </div>
             </div>
         </>
     )
