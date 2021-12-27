e6dca3124c0a03ce1affa14a72fd90e059c6a546
JunHatanaka <nextseason6@gmail.com>
2021-12-27 17:20:55 +0900

    Change: Changed head tag to components.

---
+++ components/Head.jsx
@@ -0,0 +1,14 @@
+export default function Head() {
+    return (
+        <>
+            <head>
+                <title>IdGenerator</title>
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="og:url" content="https://id-generator-plum.vercel.app/" />
+                <meta property="og:title" content="IdGenerator" />
+                <meta property="og:description" content="It is a tool that automatically generates an ID." />
+                <meta property="og:image" content="https://myproducts-hosting.s3.ap-northeast-1.amazonaws.com/images/IdGeneratorOgp.png" />
+            </head>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,13 @@ import Header from "../components/Header";
 import RandId from "../components/RandId"
 import Password from "../components/Password";
 import Footer from "../components/Footer";
+import Head from "../components/Head";
 
 export default function Home() {
   const [mode, setMode] = useState("RandId");
   return (
     <>
-      <head>
-        <title>IdGenerator</title>
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="og:url" content="https://id-generator-plum.vercel.app/" />
-        <meta property="og:title" content="IdGenerator" />
-        <meta property="og:description" content="It is a tool that automatically generates an ID." />
-        <meta property="og:image" content="https://myproducts-hosting.s3.ap-northeast-1.amazonaws.com/images/IdGeneratorOgp.png" />
-      </head>
+      <Head />
       <Header 
         setMode={setMode}
       />
