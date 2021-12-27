fde50771039ea84811f8a2f8cc58205bc5441b0e
JunHatanaka <nextseason6@gmail.com>
2021-12-27 15:27:22 +0900

    Add: Added OGP description.

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,11 @@ export default function Home() {
     <>
       <head>
         <title>IdGenerator</title>
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:url" content="https://id-generator-plum.vercel.app/" />
+        <meta property="og:title" content="IdGenerator" />
+        <meta property="og:description" content="It is a tool that automatically generates an ID." />
+        <meta property="og:image" content="https://myproducts-hosting.s3.ap-northeast-1.amazonaws.com/images/IdGeneratorOgp.png" />
       </head>
       <Header 
         setMode={setMode}
