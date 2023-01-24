43c702ada941f26b5944ed05bdffb884d712b3fd
JunHatanaka <nextseason6@gmail.com>
2023-01-25 02:22:03 +0900

    Add: Wordにreloadボタン追加

--- components/Word.jsx
+++ components/Word.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { randWord, randNumber } from "@ngneat/falso"; // https://github.com/ngneat/falso
+import { randWord } from "@ngneat/falso"; // https://github.com/ngneat/falso
 
 export default function RandPassword() {
   const [word, setWord] = useState("");
@@ -8,13 +8,16 @@ export default function RandPassword() {
     setWord(rands.word);
   }
 
+  const reloadStr = () => {
+    getWord();
+  };
+
   useEffect(() => {
     getWord();
   }, []);
   const rands = { word: randWord() };
 
   async function copyToClipboard() {
-    await getWord();
     if (process.browser) {
       const copyText = document.getElementById("copyTarget").innerText;
       await navigator.clipboard.writeText(copyText);
@@ -40,6 +43,12 @@ export default function RandPassword() {
               className="m-1 px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:bg-blue-500"
             >
               Copy
+            </button>
+            <button
+              onClick={reloadStr}
+              className="m-1 px-2 py-1 bg-blue-400 text-lg text-white font-semibold rounded hover:bg-blue-500"
+            >
+              Reload
             </button>
           </div>
         </div>
